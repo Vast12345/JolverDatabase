565c3530739d0fc67d0ddb5de1a06f44eaa6d479
Nexus <nexus@MacBook-Air.local>
2024-05-28 08:44:28 -0500

    First Initialization of Jolvers Database class

---
+++ MYSQL/Introduction/BasicCommands.sql
@@ -0,0 +1,9 @@
+insert into table_name values (...);
+
+delete from table_name where condition;
+
+update table_name
+set column1 = value1, column2 = value2, ...
+where condition;
+
+-- Like functions: A% the start, %A the end, %A% the middle.
